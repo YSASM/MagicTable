4c5511b845d8cdeb9c8842d5a8cef7c828c3835d
杳末钎散 <wujiefeng@yuan9.cn>
2023-09-25 11:08:17 +0800

    白名单bug

--- src/views/config/config/data.js
+++ src/views/config/config/data.js
@@ -6,6 +6,7 @@ let data = {
   fetchFun: api.getConfigList,
   subfromFunWhitList: (data) => {
     return new Promise((resolve, reject) => {
+      data.white_list = JSON.parse(data.white_list)
       data.white_list = data.white_list.length > 0 ? data.white_list.toLocaleString() : " "
       api.editorConfigList(data).then(res => {
         resolve(res)
